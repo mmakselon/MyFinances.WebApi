6aa9a5663d70a5315f4d00d3a2767ea747b31e19
mmakselon <magdalena.pachacz@gmail.com>
2025-01-30 21:06:25 +0100

    add Models from database

---
+++ MyFInaces.WebApi/Models/Domains/MyFinancesContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyFinances.WebApi.Models.Domains;
+
+public partial class MyFinancesContext : DbContext
+{
+    public MyFinancesContext()
+    {
+    }
+
+    public MyFinancesContext(DbContextOptions<MyFinancesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Operation> Operations { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=MyFinances;User Id=mmakselon;Password=123;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsFixedLength();
+        });
+
+        modelBuilder.Entity<Operation>(entity =>
+        {
+            entity.Property(e => e.Data).HasColumnType("datetime");
+            entity.Property(e => e.Description).HasMaxLength(500);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.Value).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Operations)
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Operations_Categories");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MyFInaces.WebApi/Models/Domains/Operation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyFinances.WebApi.Models.Domains;
+
+public partial class Operation
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public byte[]? Description { get; set; }
+
+    public decimal Value { get; set; }
+
+    public DateTime Data { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public virtual Category Category { get; set; } = null!;
+}
