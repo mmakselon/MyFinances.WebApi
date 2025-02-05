49088ff8c4f77b3b3d9b2cf9664c61e004ad0bca
mmakselon <magdalena.pachacz@gmail.com>
2025-02-05 21:53:41 +0100

    update Response class

--- MyFInaces.WebApi/Models/Response/Response.cs
+++ MyFInaces.WebApi/Models/Response/Response.cs
@@ -4,9 +4,22 @@ namespace MyFinances.WebApi.Models.Response
 {
     public class Response
     {
+        public Response()
+        {
+            Errors = new List<Error>();
+        }
         public List<Error> Errors { get; set; }
 
-        public bool IsSuccess {  get; set; }
+        public bool IsSuccess
+        {
+            get
+            {
+                return Errors == null || !Errors.Any();
+            }
+        }
+
+        //public bool IsSuccess => Errors == null || !Errors.Any(); //to samo co wyżej tylko zwięźlej
+
 
     }
 }
