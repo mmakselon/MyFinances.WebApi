902f9ca8621c994a54bfcf66d856ae1f535c0c84
mmakselon <magdalena.pachacz@gmail.com>
2025-02-11 22:14:04 +0100

    add another fix

--- MyFInaces.WebApi/Models/Domains/Operations.cs
+++ MyFInaces.WebApi/Models/Domains/Operations.cs
@@ -13,7 +13,7 @@ public partial class Operations
 
     public decimal Value { get; set; }
 
-    public DateTime Date{ get; set; }
+    public DateTime Date {get; set; }
 
     public int CategoryId { get; set; }
 
